a6f93a4972f8a7a864ab93e4e47c444b9f0acbb0
neosoob <173606415@qq.com>
2021-05-21 21:58:47 +0800

    Update __init__.py

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -8,6 +8,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import models.pose_resnet
 
 from .hrnet import get_face_alignment_net, HighResolutionNet
 
