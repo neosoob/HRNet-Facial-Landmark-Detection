b45ff90930df207e71013bec920adab132c3738a
neosoob <173606415@qq.com>
2021-05-21 15:01:04 +0800

    Update evaluation.py

--- lib/core/evaluation.py
+++ lib/core/evaluation.py
@@ -61,7 +61,7 @@ def compute_nme(preds, meta):
     return rmse
 
 
-def decode_preds(output, center, scale, res):
+def decode_preds(output, center, scale, res, rot):
     coords = get_preds(output)  # float type
 
     coords = coords.cpu()
@@ -79,7 +79,7 @@ def decode_preds(output, center, scale, res):
 
     # Transform back
     for i in range(coords.size(0)):
-        preds[i] = transform_preds(coords[i], center[i], scale[i], res)
+        preds[i] = transform_preds(coords[i], center[i], scale[i], res, rot)
 
     if preds.dim() < 3:
         preds = preds.view(1, preds.size())
